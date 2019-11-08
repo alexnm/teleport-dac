f70cdf8113c5e126adc487340f4e2dc476eaf991
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-11-08 13:45:05 +0200

    Commit made using teleportHQ

--- src/components/margin.js
+++ src/components/margin.js
@@ -5,9 +5,11 @@ import styles from './margin.module.css'
 const Margin = (props) => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading1}>Heading Level 1</h1>
+      <h1 className={styles.heading1}>Test Diffing</h1>
       <button className={styles.button}>Button</button>
-      <button className={styles.button1}>Button</button>
+      <button disabled="true" className={styles.button1}>
+        Does this really work?
+      </button>
     </div>
   )
 }
