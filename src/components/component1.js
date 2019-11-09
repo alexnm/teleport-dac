0ee778bd8e7d9e209897d1a4344bd0060343499d
Alex Moldovan <alex.n.moldovan@gmail.com>
2019-11-09 14:33:48 +0200

    Commit made using teleportHQ

---
+++ src/components/component1.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import styles from './component1.module.css'
+
+const Component1 = (props) => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.container1}></div>
+      <div className={styles.container2}></div>
+      <div className={styles.container3}></div>
+    </div>
+  )
+}
+
+export default Component1
